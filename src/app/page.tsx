303e8e796c5a543465ceb3d29ed92356d4143b70
TheGzuckert <games.zarck@gmail.com>
2024-04-28 13:23:19 -0300

    feat: new portfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,94 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import { Badge } from '@/components/ui/badge'
+import { LuSunMoon } from 'react-icons/lu'
+import { IoMdMoon } from 'react-icons/io'
+import { Card } from '@/components/card/card'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+export default function Home() {
+  const [darkmode, setDarkmode] = useState(false)
+
+  const toggleDarkMode = () => {
+    setDarkmode(!darkmode)
+  }
+
+  useEffect(() => {
+    document.body.classList.toggle('dark', darkmode)
+  }, [darkmode])
+
+  return (
+    <div className="ml-5">
+      <nav className="mt-6"></nav>
+      <header className="flex flex-col gap-3">
+        <div className="mb-3">
+          <Avatar className="w-24 h-24 flex-shrink-0 rounded-xl">
+            <AvatarImage
+              src="https://github.com/TheGzuckert.png"
+              alt="Gustavo Zuckert"
+            />
+            <AvatarFallback>Gustavo Zuckert</AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="flex gap-2">
+          <Badge
+            variant={'outline'}
+            onClick={toggleDarkMode}
+            style={{ cursor: 'pointer' }}
+          >
+            {darkmode ? (
+              <>
+                <LuSunMoon className="text-yellow-500 mr-1" />
+                Modo Escuro
+              </>
+            ) : (
+              <>
+                <IoMdMoon className="text-yellow-500 mr-1" />
+                Modo Claro
+              </>
+            )}
+          </Badge>
+
+          <Badge variant={'outline'}>TheGzuckert</Badge>
+
+          <Badge variant={'outline'}>22 - Anos</Badge>
+
+          <Badge variant={'outline'}>IFSP</Badge>
+        </div>
+        <div className="flex flex-col max-w-screen-sm">
+          <h1 className="font-bold text-3xl">Gustavo Dos Santos Zuckert</h1>
+
+          <p className="mt-1 font-bold">Fullstack Developer</p>
+
+          <p className="mt-1 mb-1 text-sm">
+            Desenvolvedor Web com seis meses de experiência no mercado.
+            Atualmente trabalho como Analista de Suporte e DBA pela Bten,
+            entretanto busco uma oportunidade de voltar para o mercado de
+            desenvolvimento.
+          </p>
+        </div>
+      </header>
+      <section className="mt-4 max-w-screen-sm mr-4">
+        <div>
+          <p className="font-bold">💼 Experiencias profissionais</p>
+          <Card
+            company="Bten"
+            date="Oct 17, 2022 - Atualmente"
+            title="Analista de Suporte e DBA"
+            location="Remoto"
+            image="/BtenIcon.jpg"
+            fallbackImage="BtenIcon.jpg"
+          />
+          <Card
+            company="Corelab"
+            date="Fev 28, 2022 - 01 Jul 2022"
+            title="Desenvolvedor Web -  Treinee"
+            location="Remoto"
+            image="/Corelab.jpg"
+            fallbackImage="Corelab.jpg"
+          />
+        </div>
+      </section>
+    </div>
+  )
+}
---
+++ src/components/card/card.tsx
@@ -0,0 +1,45 @@
+import { Badge } from '@/components/ui/badge'
+import React from 'react'
+import { Avatar } from '../ui/avatar'
+import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
+
+interface CardProps {
+  company: string
+  title: string
+  location: string
+  date: string
+  image?: string
+  fallbackImage?: string
+}
+
+export const Card = ({
+  location,
+  company,
+  title,
+  date,
+  image,
+  fallbackImage,
+}: CardProps) => {
+  return (
+    <div className="cursor-pointer mt-2 rounded-sm bg-card/25 text-foreground hover:bg-secondary/50 border p-4 backdrop-brightness-110 transition-all duration-150 dark:shadow-black/15 flex items-start">
+      <span className="mr-4">
+        <Avatar className="mb-2 flex flex-shrink-0 rounded-xl">
+          <AvatarImage src={image} alt={company}></AvatarImage>
+          <AvatarFallback>{fallbackImage}</AvatarFallback>
+        </Avatar>
+      </span>
+      <div className="flex flex-col justify-between flex-grow">
+        <div className="flex justify-between items-center mb-2">
+          <Badge>{company}</Badge>
+          <p className="text-sm text-gray-400">{date}</p>
+        </div>
+        <div className="flex flex-col">
+          <p>{title}</p>
+          <h2 className="text-sm text-gray-400">{location}</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
