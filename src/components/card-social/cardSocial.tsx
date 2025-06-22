3e40f0b597b5033f330dc799e9fe45ddc22261cb
TheGzuckert <games.zarck@gmail.com>
2025-06-22 19:00:46 -0300

    fix: card social error in deploy

--- src/components/card-social/cardSocial.tsx
+++ src/components/card-social/cardSocial.tsx
@@ -7,7 +7,10 @@ import Link from 'next/link'
 export function CardSocial(props: Social) {
   return (
     <div className="cursor-pointer mb-2 rounded-sm bg-card/25 text-foreground hover:bg-secondary/50 border p-4 sm:p-7 mr-2 backdrop-brightness-110 transition-all duration-150 dark:shadow-black/15">
-      <Link href={props.link} className="flex flex-col items-center sm:items-start">
+      <Link
+        href={props.link}
+        className="flex flex-col items-center sm:items-start"
+      >
         <Avatar className="rounded-xl w-12 h-12 sm:w-16 sm:h-16">
           <AvatarImage src={props.image}></AvatarImage>
           <AvatarFallback>{props.fallbackImage}</AvatarFallback>
